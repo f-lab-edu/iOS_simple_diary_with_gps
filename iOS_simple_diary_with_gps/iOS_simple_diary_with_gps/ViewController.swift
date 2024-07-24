7ae61ce62e4e20de709e2c5f0b732fa7cf17a878
Lemon Kim <49704137+lemon-kim@users.noreply.github.com>
2024-07-24 22:41:40 +0900

    init commit

---
+++ iOS_simple_diary_with_gps/iOS_simple_diary_with_gps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_simple_diary_with_gps
+//
+//  Created by 김보라 on 7/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
